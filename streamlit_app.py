8572f1ad470a7ba77500a4069ae073fb81698772
Guljeet Singh <88306751+gulsingh8@users.noreply.github.com>
2023-05-31 12:44:52 +0530

    Update streamlit_app.py add new fruit by user

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,3 +46,7 @@ streamlit.text("Hello from Snowflake:")
 streamlit.header("The Fruit Load List Contains:")
 # streamlit.text(my_data_rows)
 streamlit.dataframe(my_data_rows)
+
+# allow end user to add a fruit to the list
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
+streamlit.write('Thanks for adding ', fruit_choice)
